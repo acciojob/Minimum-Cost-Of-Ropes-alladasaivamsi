2564c6a7f26b2e6125db0363d4092685a249b55a
alladasaivamsi <109912485+alladasaivamsi@users.noreply.github.com>
2023-02-17 17:36:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 //your code here
+function minCost(event) {
+	event.preventDefault();
+
+	var input = document.querySelector("input").value;
+	var arr = input.split(",");
+	var cost = 0;
+	while(arr.length > 1)
+		{
+			Array.sort(function(a,b)
+			{
+				return a-b
+			});
+
+			var temp = Number(arr[0])+ Number(arr[1]);
+			cost += temp;
+			arr.push(temp);
+			arr.slice(0,2);
+			arr.sort(function(a,b)
+			{
+				return a-b
+			});
+		}
+
+		document.querySelector("div").textContent = cost;
+}
